c77e0cfeaacceb8ccf5186c1f882db547a7a46fe
Gukisz <gustavoribeiro070@gmail.com>
2024-11-13 19:03:29 -0300

    Adição de um histórico de operações para armazenar os últimos cálculos realizados.

---
+++ Historico.ts
@@ -0,0 +1,11 @@
+export class Historico {
+    private history: string[] = [];
+  
+    adicionarEntrada(entry: string): void {
+      this.history.push(entry);
+    }
+  
+    getHistorico(): string[] {
+      return this.history;
+    }
+}
